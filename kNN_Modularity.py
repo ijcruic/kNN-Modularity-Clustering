1ae856bcde856a42e64f8c9935c755b42cf25d97
Iain <iain.j.cruickshank@gmail.com>
2018-05-04 14:17:47 -0400

    Update kNN_Modularity.py

--- kNN_Modularity.py
+++ kNN_Modularity.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  4 10:33:33 2018
+Created on Wed May  2 15:55:35 2018
 
 @author: icruicks
 """
 
-import pandas as pd, numpy as np, networkx as nx, os
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
 import community
-from matplotlib import pyplot
 
-def kNetwork(frequencies):
+
+def kNN_Modularity(distances):
     bestModularity =-np.infty
     bestNetwork = nx.Graph()
     bestPart =[]
     kBest =2
-    np.fill_diagonal(frequencies, -np.infty)
+    np.fill_diagonal(distances, np.infty)
     
-    for n in range(1, np.int(np.floor(np.log2(frequencies.shape[0])))):
+    for n in range(1, np.int(np.floor(np.log2(distances.shape[0])))):
         k = 2**n
-        nodes = np.argpartition(frequencies, -3)[:,-3:]
+        nodes = np.argpartition(distances, k)[:,:k]
         edges = []
         for i in range(nodes.shape[0]):
             for j in nodes[i,:]:
@@ -39,15 +41,11 @@ def kNetwork(frequencies):
     return np.array([bestPart[i] for i in range(nodes.shape[0])]), kBest, bestModularity, bestNetwork
 
 
-frequencyMatrix = pd.read_csv(os.path.join("Data","SCM_Frequency_Network.csv"))
-labels = frequencyMatrix.iloc[:,0]
-subGroupsDF = pd.DataFrame({'Nodes':labels})
-networkDF = pd.DataFrame(index=labels)
-subGroupsDF['sub groups'], k, modularity, network = kNetwork(frequencyMatrix.iloc[:,1:].as_matrix())
+affinity_test =np.random.rand(1000,1000)
 
-nx.draw(network)
-nx.draw_networkx_labels(network, pos=nx.spring_layout(network), labels=labels.to_dict())
+groups, k , modularity, graph = kNN_Modularity(affinity_test)
 
-networkDF = nx.to_pandas_adjacency(nx.relabel_nodes(network, labels.to_dict()))
-networkDF.to_csv(os.path.join("Data","SCM_latent_network.csv"))
-subGroupsDF.to_csv(os.path.join("Data","SCM_louvain_groups.csv"))
+print(groups)
+print('\n')
+print('Best Modularity: {}'.format(modularity))
+nx.draw_networkx(graph)

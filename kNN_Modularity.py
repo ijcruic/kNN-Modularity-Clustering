2ccaf738d9086e2b4b8a03613a8e526f7269e80c
Iain <iain.j.cruickshank@gmail.com>
2018-05-15 10:19:46 -0400

    Placed into sklearn cluster format

--- kNN_Modularity.py
+++ kNN_Modularity.py
@@ -4,48 +4,40 @@ Created on Wed May  2 15:55:35 2018
 
 @author: icruicks
 """
-
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
+from sklearn.base import BaseEstimator, ClusterMixin
+import numpy as np, networkx as nx
 import community
 
+class kNetwork(BaseEstimator, ClusterMixin):
 
-def kNN_Modularity(distances):
-    bestModularity =-np.infty
-    bestNetwork = nx.Graph()
-    bestPart =[]
-    kBest =2
-    np.fill_diagonal(distances, np.infty)
+    def __init__(self):
+         self.bestModularity =-np.infty
+         self.bestNetwork = nx.Graph()
+         self.bestPart =[]
+         self.kBest =2
+         
+    def fit_predict(self, distances):
+         
+        np.fill_diagonal(distances, np.infty)
     
-    for n in range(1, np.int(np.floor(np.log2(distances.shape[0])))):
-        k = 2**n
-        nodes = np.argpartition(distances, k)[:,:k]
-        edges = []
-        for i in range(nodes.shape[0]):
-            for j in nodes[i,:]:
-                edges.append((i,j))
-                
-        network = nx.Graph()
-        network.add_nodes_from(nodes[:,0])
-        network.add_edges_from(edges)
-        
-        currPart = community.best_partition(network)
-        currModularity = community.modularity(currPart, network)
-        if currModularity > bestModularity:
-            bestModularity = currModularity
-            bestNetwork = network
-            bestPart = currPart
-            kBest = k
+        for n in range(1, np.int(np.floor(np.log2(distances.shape[0])))):
+            k = 2**n
+            nodes = np.argpartition(distances, k)[:,:k]
+            edges = []
+            for i in range(nodes.shape[0]):
+                for j in nodes[i,:]:
+                    edges.append((i,j))
+                    
+            network = nx.Graph()
+            network.add_nodes_from(nodes[:,0])
+            network.add_edges_from(edges)
+            
+            currPart = community.best_partition(network)
+            currModularity = community.modularity(currPart, network)
+            if currModularity > self.bestModularity:
+                self.bestModularity = currModularity
+                self.bestNetwork = network
+                self.bestPart = currPart
+                self.kBest = k
 
-    return np.array([bestPart[i] for i in range(nodes.shape[0])]), kBest, bestModularity, bestNetwork
-
-
-affinity_test =np.random.rand(1000,1000)
-
-groups, k , modularity, graph = kNN_Modularity(affinity_test)
-
-print(groups)
-print('\n')
-print('Best Modularity: {}'.format(modularity))
-nx.draw_networkx(graph)
+        return np.array([self.bestPart[i] for i in range(nodes.shape[0])]), self.kBest, self.bestModularity, self.bestNetwork

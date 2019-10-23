cf34037a03194648416feb80e6694b31c9688eac
Iain <iain.j.cruickshank@gmail.com>
2019-10-23 10:30:17 -0400

    Major Changes to Louvain Clustering
    Changed to sknetwork version of Louvain clusteirng to allow for use of driected networks in the graph leanring framework. Also changed the randomization of the network for better results.

--- kNN_Modularity.py
+++ kNN_Modularity.py
@@ -7,45 +7,49 @@ Created on Fri May  4 10:33:33 2018
 from sklearn.base import BaseEstimator, ClusterMixin
 from sklearn.neighbors import kneighbors_graph
 import numpy as np, networkx as nx
-import community
+from sknetwork.clustering import Louvain, modularity
 
 class kNN_network(BaseEstimator, ClusterMixin):
 
-    def __init__(self, metric='euclidean', symmetrize=True):
-         self.bestModularity =-1
-         self.bestNetwork = nx.Graph()
-         self.bestPart =[]
-         self.kBest =2
+    def __init__(self, metric='euclidean', graph_type='symmetric'):
+         self.best_modularity =-1
+         self.best_network = nx.Graph()
+         self.best_part =[]
+         self.k_best =2
          self.metric=metric
-         self.symmetrize=symmetrize
+         self.graph_type = graph_type
          
     def fit_predict(self, X):
     
         for n in range(1, np.int(np.floor(np.log2(X.shape[0])))):
             k = 2**n
             kNN = kneighbors_graph(X, metric=self.metric, mode='connectivity', n_neighbors=int(k))
-            if self.symmetrize:
+            if self.graph_type == 'symmetric':
                 kNN = kNN.minimum(kNN.T)
-            else:
+            elif self.graph_type=='assymetric':
                 kNN = kNN.maximum(kNN.T)
             network = nx.from_scipy_sparse_matrix(kNN)
-            currPart = community.best_partition(network)
-            randomNetwork = self._randomize_network(network)
-            currRandomPart = community.best_partition(randomNetwork)
-            currModularity = community.modularity(currPart, network) - community.modularity(currRandomPart, randomNetwork)
-            #currModularity = community.modularity(currPart, network) - self._random_modularity(network)
-            if currModularity > self.bestModularity:
-                self.bestModularity = currModularity
-                self.bestNetwork = network
-                self.bestPart = currPart
-                self.kBest = k
+            clstr = Louvain()
+            curr_part = clstr.fit(kNN).labels_
+            random_kNN = nx.to_scipy_sparse_matrix(self._randomize_network(network, self.graph_type))
+            curr_random_part = clstr.fit(random_kNN).labels_
+            curr_modularity = modularity(kNN, curr_part) - modularity(random_kNN, curr_random_part)
+            #curr_modularity = modularity(kNN, curr_part) - self._random_modularity(network)
+            if curr_modularity > self.best_modularity:
+                self.best_modularity = curr_modularity
+                self.best_network = network
+                self.best_part = curr_part
+                self.best_k = k
 
-        return np.array(list(self.bestPart.values()))
+        return self.best_part
     
-    def _randomize_network(self, network):
-        adj_matrix = nx.to_numpy_matrix(network)
-        np.transpose(np.random.shuffle(np.transpose(adj_matrix)))
-        return nx.from_numpy_matrix(adj_matrix)
+    def _randomize_network(self, network, graph_type):
+        n = network.number_of_nodes()
+        p = nx.density(network)
+        if graph_type =='directed':
+            return nx.fast_gnp_random_graph(n, p, directed=True)
+        else:
+            return nx.fast_gnp_random_graph(n, p, directed=False)
     
     def _random_modularity(self, network):
         S = network.number_of_nodes()
--- kNN_Modularity_Test.py
+++ kNN_Modularity_Test.py
@@ -8,7 +8,7 @@ Created on Mon May 14 14:42:17 2018
 import pandas as pd, networkx as nx, kNN_Modularity
 from matplotlib import pyplot as plt
 from sklearn import datasets
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import adjusted_mutual_info_score
 
 iris = datasets.load_iris()
 X = iris.data
@@ -18,12 +18,12 @@ subGroupsDF = pd.DataFrame({'Nodes':y})
 subGroupsDF.replace(to_replace={0:'Setosa', 1:'Versicolour',
                           2:'Virginica'}, inplace=True)
 
-kNN = kNN_Modularity.kNN_network(metric='euclidean', symmetrize=False)
+kNN = kNN_Modularity.kNN_network(metric='euclidean', graph_type='symmetric')
 subgroups = kNN.fit_predict(X)
-latent_network = kNN.bestNetwork
+latent_network = kNN.best_network
 
 
-print("The accuracy of k-NN Modularity Maximization on cluster label is: {}".format(accuracy_score(y, subgroups)))
+print("The Adjusted Mutyal Information of k-NN Modularity Maximization on cluster label is: {}".format(adjusted_mutual_info_score(y, subgroups)))
 
 labels = subGroupsDF['Nodes']
 nx.draw(latent_network, node_color=subgroups, cmap=plt.cm.Set1, labels=labels)
